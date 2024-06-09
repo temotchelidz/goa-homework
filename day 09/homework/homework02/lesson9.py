1913a983d4e03d77baf0a5555c12ffa3236fbc0a
temotchelidz <temo31442@gmail.com>
2024-06-09 22:45:05 +0300

    eighth commit

---
+++ day 09/homework/homework02/lesson9.py
@@ -0,0 +1,17 @@
+x = int (input ("enter x: "))
+
+y = int  (input ("enter y: "))
+
+h = int (input ("enter h: "))
+
+print(x + y / h)
+print(x * y + h)
+print(x + h + h)
+print(x * y + h)
+print(x - y * h)
+print(x + y + h)
+print(x * y / h)
+print(x / y + h)
+print(x * y * h)
+print(x + y - h)
+print(x + y - h)
---
+++ day 09/homework/lesson09.py
@@ -0,0 +1,20 @@
+#homework N1
+name = input ("what is your name:")
+print(name)
+
+last_name = input ("what is your last name:")
+print(last_name)
+
+age = input ("what is your age:")
+print(age)
+
+country = input ("where do you live:")
+print(country)
+
+color = input ("what is your favorite color:")
+print(color)
+
+number = input ("what is your favorite number:")
+print(number)
+
+
