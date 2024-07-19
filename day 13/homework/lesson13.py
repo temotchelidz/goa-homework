55216dcd6bd27e1b74154c6ab048fb39f69819d8
temotchelidz <temo31443@gmail.com>
2024-07-19 16:27:17 +0400

    thirteenth commit

---
+++ day 13/homework/lesson13.py
@@ -0,0 +1,38 @@
+#1) მომხმარებელს შემოატანინეთ 4 სხავდასხვა რიცხვი და ამ ოთხივე რიცხვზე მოახდინეთ არითმეტიკური ოპერაციები. +,-,*,//,**
+
+#x = int(input("enter x: " ))
+#a = int(input(" enter a: "))
+#w = int(input(" enter w: "))
+
+
+#print(x + a / w)
+#print(x * a + w)
+#print(x / a +w )
+#print(x - a * w)
+#print(x + a + w)
+#print(x * a / w)
+#print(x * a * w)
+#print(x + a - w)
+#print(x - a - w )
+
+#2) დაწერეთ პროგრამა სადაც შემოიტანთ თქვენი ოჯახის წევრების ასაკს და გამოიტანეთ ის თუ რამდენი წლის იქნებიან 20 წლის შემდეგ.
+
+
+#mom_age = int(input("enter mom name:"))
+#print(mom_age + 20)
+
+
+
+#3) შექმენით ცვლადები სადაც შეინახავთ თქვენს თავზე ინფორმაციას (სახელი, გვარი, ასაკი, ქვეყანა, ქალაქი, საყვარელი ფერი, საყვარელი მანქანა, საყვარელი საჭმელი, საყვარელი სპორტი და ა.შ) შემდეგ შემოტანილი მნიშვნელობებით ააწყვეტ ერთი დიდი წინადადება.
+
+#name = input(" enter your name: ")
+#last_name = input(" enter your last name: ") 
+#country = input(" which country do you live: ")
+#fav_car = input (" enter your fv car: ") 
+
+#print(" hello my name is " + name + " my last name " + last_name + " i live in " + country + " your favourite car is " + fav_car)
+
+name = input(" enter name: ")
+print("happy birthday",name)
+
+
