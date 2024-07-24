47bfacea210ac32ffee44348515e69c1073e87fb
temotchelidz <temo31443@gmail.com>
2024-07-24 20:56:04 +0400

    დავალება3: კომენტარის სახით ახსენით როგორ მუშაობს and და or ოპერატორი და დაბლა დაუწერეთ თითო მაგალითი

---
+++ day 15/homework/lesson15.py
@@ -0,0 +1,28 @@
+#დავალება1: გაკეთეთ and და or ოპერატორებზე 10 მაგალითი.
+
+
+a =  10
+b = 15
+f = 20
+g = 25
+print(a < b  or f > g)
+
+c = 20
+v = 30
+h = 33
+o = 60
+
+print(c > v and h < o)
+
+
+
+step_taken = int (input("enter number of step you have take  in 10m>"))
+
+has_taken_enught_steps = step_taken >=1000
+
+print(has_taken_enught_steps)
+
+
+
+
+
