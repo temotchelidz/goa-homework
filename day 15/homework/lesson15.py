74f1f942d3632d272f22502e12d76c4ddd2e2df7
temotchelidz <temo31443@gmail.com>
2024-07-25 01:32:26 +0400

    sixteenth commit

--- day 15/homework/lesson15.py
+++ day 15/homework/lesson15.py
@@ -15,12 +15,21 @@ o = 60
 print(c > v and h < o)
 
 
+#დავალება2: წარმოიდგინეთ ხართ მორბენალი, თქვენი დავალება შეამოწმოთ 10 წუთში თუ გადადგით 1000 ნაბიჯი, უნდა გამოიტანოს ტერმინალში ან True ან False. მინიშნება: უნდა გამოიყენოთ ინფუთები, და and და or ოპერატორები.
+
 
 step_taken = int (input("enter number of step you have take  in 10m>"))
 
 has_taken_enught_steps = step_taken >=1000
 
 print(has_taken_enught_steps)
+
+
+#დავალება3: კომენტარის სახით ახსენით როგორ მუშაობს and და or ოპერატორი და დაბლა დაუწერეთ თითო მაგალითი
+
+#the and operator returns true if both conditions are true, otherwise it returns false
+#the or operator returns true if at least ine of condition is true, otherwise it returns false
+
 
 
 
