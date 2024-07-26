36dd4394769c397fb2db698d22c385d3c317f42e
temotchelidz <temo31443@gmail.com>
2024-07-27 02:03:09 +0400

    seventeenth commit

---
+++ day 17/homework/lesson17.py
@@ -0,0 +1,22 @@
+
+#დავალება2: მომხმარებელს შემოატანინეთ 2 რიცხვი, და შემდეგ მოახდინეთ ამ ორ რიცხვს შორის მათემატიკური ოპერაცია.
+
+#b =int(input("enter num:"))
+
+#a =int(input("enter num2:"))
+
+#print(b + a)
+#print(b / a )
+#print(b - a)
+#print(b * a)
+
+
+#დავალება3: შექმენით 2 ინფუთ, და შემოიტანეთ თქვენი სკოლის ნიშნები, შემდეგ კი გამოითვალეთ ამ შემოტანილი რიცხვების, საშვალო არითმეტიკული.
+
+music = int(input("enter music score: "))
+english =int(input("enter english score: "))
+math =int(input("enter math score: ")) 
+science =int(input("enter science score: "))
+print((music + english + math + science) // 4) 
+
+
