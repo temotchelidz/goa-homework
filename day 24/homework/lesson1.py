0caede4024cfb7092b850e076adb818537a64375
temotchelidz <168912309+temotchelidz@users.noreply.github.com>
2024-08-30 00:31:24 +0400

    Add files via upload

---
+++ day 24/homework/lesson1.py
@@ -0,0 +1,22 @@
+for i in range(29+1):
+   print(" GOA ")
+
+for i in range(20):
+   print("GOA ORIENTED ACADEMY")
+
+for i in range(15):
+   print(" hello world ")
+
+
+   for i in range(44):
+     print(" temo ")
+
+
+number = int(input("enter number:"))
+my_number =int(input(" number"))
+while number != my_number:
+   my_num =int(input("my number"))
+
+
+
+
